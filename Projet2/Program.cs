7c040f355fd1a6ec6e127298931184227b195894
camille1407 <edohcamille7@gmail.com>
2021-08-03 12:31:23 +0000

    exercice de c#

---
+++ Projet2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Projet2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double longueur, largeur;
+            Console.Out.WriteLine("\n\nRectangle 1 = ");
+            Console.Out.Write("Donner la longueur du rectangle: ");
+            longueur = double.Parse(Console.In.ReadLine());
+            Console.Out.Write("Donner la largeur du rectangle: ");
+            largeur = double.Parse(Console.In.ReadLine());
+            Rectangle R1 = new Rectangle(longueur, largeur);
+            Console.Out.WriteLine("Le périmétre est " + R1.Perimetre());
+            Console.Out.WriteLine("La surface est " + R1.Aire());
+            if (R1.EstCarre())
+                Console.Out.WriteLine("C'est un carré");
+            else
+                Console.Out.WriteLine("Ce n'est pas un carré");
+
+            Console.Out.WriteLine("\n\nRectangle 2 = ");
+            Console.Out.Write("Donner la longueur du rectangle: ");
+            longueur = double.Parse(Console.In.ReadLine());
+            Console.Out.Write("Donner la largeur du rectangle: ");
+            largeur = double.Parse(Console.In.ReadLine());
+
+            Rectangle R2 = new Rectangle();
+            R2.Longueur = longueur;
+            R2.Largeur = largeur;
+            R2.AfficherRectangle();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projet2/Rectangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet2
+{
+    class Rectangle
+    {
+        private double longueur;
+        private double largeur;
+        public double Longueur
+        {
+            get { return longueur; }
+            set { longueur = value; }
+        }
+
+        public double Largeur
+        {
+            get { return largeur; }
+            set { largeur = value; }
+        }
+
+       
+
+        public Rectangle() { }
+        public Rectangle(double lon, double larg)
+        {
+            this.longueur = lon;
+            this.largeur = larg;
+        }
+
+        public double Perimetre()
+        {
+            return 2 * (longueur + largeur);
+        }
+
+        public double Aire()
+        {
+            return longueur * largeur;
+        }
+
+        public bool EstCarre()
+        {
+            if (longueur == largeur)
+                return true;
+            else
+                return false;
+        }
+
+        public void AfficherRectangle()
+        {
+            string s = "";
+            s += "- Longueur : " + longueur;
+            s += "- Largeur : " + largeur;
+            s += "- Périmétre : " + Perimetre();
+            s += "- Aire : " + Aire();
+            if (EstCarre())
+                s += "- Il s'agit d'un carré";
+            else
+                s += "- Il ne s'agit pas d'un carré";
+
+            Console.Out.WriteLine(s);
+        }
+    }
+}
